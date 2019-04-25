a1e316e71816ea2e7aa8d7991e119512a6921ead
t-mitani <t.mitani1988@gmail.com>
2019-04-25 23:59:19 +0900

    test fail

--- src/test/java/com/example/javamavenjunithelloworld/HelloAppTest.java
+++ src/test/java/com/example/javamavenjunithelloworld/HelloAppTest.java
@@ -52,7 +52,8 @@ public class HelloAppTest {
             fail("Unreachable.");
         } catch (TestExitException e) {
             // Did the program exit with the expected error code?
-            assertThat(e.getStatus(), is(HelloApp.EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD));
+            // assertThat(e.getStatus(), is(HelloApp.EXIT_STATUS_PARAMETER_NOT_UNDERSTOOD));
+            assertThat(e.getStatus(), is(HelloApp.EXIT_STATUS_HELLO_FAILED));
         }
     }
 
